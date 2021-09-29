70443fe49d362fd40317136801911b35265467c5
Eric860730 <s12345d60@gmail.com>
2021-09-29 19:11:44 +0800

    [Feat] Check the number of arguments.

--- hw1/main.py
+++ hw1/main.py
@@ -1,11 +1,11 @@
 import sys
 import ml
 
-
 def main():
     ml.R_Regression()
 
-
-
 if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Invalid argument number!\nUsage:\n$ python3 main.py <csvfile> <n> <lambda>")
+        exit()
     main()
