45881bb7aef3058820ab02246fb7f71d60176cad
Eric860730 <s12345d60@gmail.com>
2021-10-03 23:30:42 +0800

    [HW2] Finish Question 1. [Feat] Add a model directory to store models. [Feat] Add continuous mode to the Naive_Bayes_classifier.py.

--- hw2/main.py
+++ hw2/main.py
@@ -3,8 +3,11 @@ import ml
 
 
 def main():
+    if (len(sys.argv) != 2):
+        print("Please Enter mode.")
+        exit()
     train_label, train_image, test_label, test_image = ml.read_data('./hw2_data/')
-    ml.calculateNaiveBayesClassifier(train_label, train_image, test_label, test_image)
+    ml.calculateNaiveBayesClassifier(train_label, train_image, test_label, test_image, int(sys.argv[1]))
 
 
 if __name__ == '__main__':
--- hw2/ml/Naive_Bayes_classifier.py
+++ hw2/ml/Naive_Bayes_classifier.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 # python rich for debug
 from rich.traceback import install
@@ -6,6 +7,7 @@ install(show_locals=True)
 
 CLASS_NUM = 10
 BIN_VALUE_RANGE = 32
+PI = 3.14159265359
 
 
 def calculatePrior(train_label):
@@ -55,7 +57,7 @@ def calculateLikelihood(train_label, train_image):
 
 def predictInDiscreteMode(prior, likelihood, test_label, test_image):
     error_num = 0
-    pixel = len(test_image[1])
+    pixel = len(test_image[0])
     test_image_num = len(test_image)
     for i in range(test_image_num):
         # calculate posterior
@@ -80,11 +82,40 @@ def predictInDiscreteMode(prior, likelihood, test_label, test_image):
     return error_rate
 
 
+def predictInContinuousMode(mean, variance, prior, test_label, test_image):
+    error_num = 0
+    pixel = len(test_image[0])
+    test_image_num = len(test_image)
+    for i in range(test_image_num):
+        posterior = np.zeros(CLASS_NUM, dtype=float)
+        for j in range(CLASS_NUM):
+            for k in range(pixel):
+                if variance[j][k] == 0:
+                    continue
+                posterior[j] -= np.log(2.0 * PI * variance[j][k]) / 2.0
+                posterior[j] -= ((test_image[i][k] - mean[j][k])
+                                 ** 2.0) / (2.0 * variance[j][k])
+            posterior[j] += np.log(prior[j])
+
+        posterior_sum = np.sum(posterior)
+        posterior[:] = posterior[:] / posterior_sum
+        print("Postirior (in log scale):")
+        for k in range(CLASS_NUM):
+            print(f"{k}: {posterior[k]}")
+        predict = np.argmin(posterior)
+        print(f"Prediction: {predict}, Ans: {test_label[i]}\n")
+
+        if(predict != test_label[i]):
+            error_num += 1
+    error_rate = error_num / test_image_num
+    return error_rate
+
+
 def showDiscreteImagination(likelihood):
     zero = np.sum(likelihood[:, :, 0:16], axis=2)
     one = np.sum(likelihood[:, :, 16:32], axis=2)
     pixel = len(likelihood[1])
-    imagination = np.zeros((CLASS_NUM, pixel), dtype = int)
+    imagination = np.zeros((CLASS_NUM, pixel), dtype=int)
     for i in range(CLASS_NUM):
         for j in range(pixel):
             if(one[i][j] > zero[i][j]):
@@ -102,19 +133,98 @@ def showDiscreteImagination(likelihood):
         print()
 
 
+def showContinuousImagination(mean):
+    pixel = len(mean[1])
+    imagination = np.zeros((CLASS_NUM, pixel), dtype=int)
+    for i in range(CLASS_NUM):
+        for j in range(pixel):
+            if(mean[i][j] >= 128):
+                imagination[i][j] = 1
+
+    print("Imagination of numbers in Bayesian classifier:\n")
+    row = int(pixel ** 0.5)
+    col = row
+    for i in range(CLASS_NUM):
+        print(f"{i}:")
+        for j in range(row):
+            for k in range(col):
+                print(f"{imagination[i][j*row + k]} ", end='')
+            print()
+        print()
+
+
+def calculateMeanAndVariance(train_label, train_image, prior):
+    pixel = len(train_image[0])
+    train_num = len(train_image)
+    mean = np.zeros((CLASS_NUM, pixel), dtype=float)
+    variance = np.zeros((CLASS_NUM, pixel), dtype=float)
+
+    # calculate mean
+    for i in range(train_num):
+        for j in range(pixel):
+            mean[train_label[i]][j] += train_image[i][j]
+    for i in range(CLASS_NUM):
+        mean[i][:] /= (prior[i] * train_num)
+
+    # calculate E(x^2)
+    E_x2 = np.zeros((CLASS_NUM, pixel), dtype=float)
+    for i in range(train_num):
+        for j in range(pixel):
+            E_x2[train_label[i]][j] += (train_image[i][j] ** 2)
+    for i in range(CLASS_NUM):
+        E_x2[i][:] /= (prior[i] * train_num)
+
+    # calculate (E(x))^2
+    Ex_2 = mean ** 2
+
+    # calculate variance
+    variance = E_x2 - Ex_2
+
+    return mean, variance
+
+
 def calculateNaiveBayesClassifier(
         train_label,
         train_image,
         test_label,
-        test_image):
-    #prior = calculatePrior(train_label)
-    #likelihood = calculateLikelihood(train_label, train_image)
-    #np.save("prior.npy", prior)
-    #np.save("likelihood.npy", likelihood)
-    prior = np.load("prior.npy")
-    likelihood = np.load("likelihood.npy")
-    error_rate = predictInDiscreteMode(
-        prior, likelihood, test_label, test_image)
-    showDiscreteImagination(likelihood)
-    print(f"Error rate: {error_rate}")
-    exit()
+        test_image,
+        mode):
+
+    # discrete mode
+    if (mode == 0):
+        if ((os.path.exists("model/prior.npy")
+                and (os.path.exists("model/likelihood.npy")))):
+            prior = np.load("model/prior.npy")
+            likelihood = np.load("model/likelihood.npy")
+        else:
+            prior = calculatePrior(train_label)
+            likelihood = calculateLikelihood(train_label, train_image)
+            np.save("model/prior.npy", prior)
+            np.save("model/likelihood.npy", likelihood)
+        error_rate = predictInDiscreteMode(
+            prior, likelihood, test_label, test_image)
+        showDiscreteImagination(likelihood)
+        print(f"Error rate: {error_rate}")
+        exit()
+    # continuous mode
+    elif (mode == 1):
+        if (os.path.exists("model/prior.npy") and (os.path.exists("model/mean.npy"))
+                and (os.path.exists("model/variance.npy"))):
+            prior = np.load("model/prior.npy")
+            mean = np.load("model/mean.npy")
+            variance = np.load("model/variance.npy")
+        else:
+            prior = calculatePrior(train_label)
+            mean, variance = calculateMeanAndVariance(
+                train_label, train_image, prior)
+            np.save("model/mean.npy", mean)
+            np.save("model/variance.npy", variance)
+            np.save("model/prior.npy", prior)
+        error_rate = predictInContinuousMode(
+            mean, variance, prior, test_label, test_image)
+        showContinuousImagination(mean)
+        print(f"Error rate: {error_rate}")
+        exit()
+    else:
+        print("Invalid mode!")
+        exit()
