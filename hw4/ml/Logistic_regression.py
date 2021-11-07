8006afadcaf6951ac62ca0ea3be8acdc0c5f733d
Eric860730 <s12345d60@gmail.com>
2021-11-07 22:06:40 +0800

    [HW4] part1 still have bug QAQ.

--- hw4/ml/Logistic_regression.py
+++ hw4/ml/Logistic_regression.py
@@ -1,7 +1,13 @@
 import ml
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.special import expit
+from scipy.linalg import inv
 import math
+
+#python rich for debug
+from rich.traceback import install
+install(show_locals=True)
 
 
 def creatDesignMatrix(Data, n):
@@ -90,11 +96,105 @@ def Gradient_descent(X, Y):
     count = 0
     while True:
         count += 1
-        # Calculate ∇wJ = A^T(Yi - (1 + e^-Xiw) ^ -1)
-        gradientJ = np.matmul(X.transpose(), Y - np.linalg.inv(1 + math.exp(np.matmul(-X, w))))
+        # Calculate ∇wJ = X^T(Yi - 1 / (1 + e^-Xiw))
+        # scipy.special.expit(x) = 1 / (1 + exp(-x))
+        gradientJ = np.matmul(X.transpose(), Y - expit(np.matmul(X, w)))
         if (abs(gradientJ) < 1e-5).all() or count > 10000:
             return w
         w = w + gradientJ
+
+
+# Newton's method: x1 = x0 - f'(x0) / f''(x0) = x0 - Hf(x)^-1 * ∇f(x0), where f(x) = J
+# H(J) = X^T * D * X, where Djj = e^-Xijwj/(1+e^-Xijwj)^2 = [1/(1+e^-Xijwj)] * [1 - 1/(1+e^-Xijwj)]
+# ∇wJ = X^T(Yi - 1 / (1 + e^-Xiw))
+def calculateNewtonRegression(X, Y, n):
+    # suppose basis is 2, given arbitrary w.
+    w = np.random.rand(3, 1)
+    D = np.zeros((2*n, 2*n))
+    count = 0
+    while True:
+        count += 1
+        old_w = w
+        # logistic function logis_f = 1/(1+e^-Xijwj)
+        logis_f = expit(np.matmul(X, w))
+        gradientJ = np.matmul(X.transpose(), Y - logis_f)
+        for i in range(2*n):
+            D[i][i] = np.matmul(logis_f[i], 1 - logis_f[i])
+        Hessian_f = np.matmul(np.matmul(np.transpose(X), D), X)
+        # if Hessian function is none singular, calculate new_w = w + H(J)^-1 * ∇J
+        try:
+            new_w = w + np.matmul(inv(Hessian_f), gradientJ)
+        # else, use steepest gradient descent
+        except:
+            new_w = w + gradientJ
+        w = new_w
+        if (abs((w - old_w) < 1e-5).all() or count > 100000):
+            return w
+
+
+# According our regression result w to classify X
+def classifyResult(X, w):
+    class1 = []
+    class2 = []
+    n = X.shape[0] / 2
+    # calculate the number of successful classifications s1 and s2
+    s1 = 0
+    s2 = 0
+    for i in range(X.shape[0]):
+        if np.matmul(X[i], w) < 0:
+            class1.append(X[i, 0:2])
+            if i < n:
+                s1 += 1
+        else:
+            class2.append(X[i, 0:2])
+            if i >= n:
+                s2 +=1
+    class1 = np.array(class1)
+    class2 = np.array(class2)
+    return s1, s2, class1, class2
+
+
+def visualizeConfusionMatrix(s1, s2, n):
+    print(f"Confusion Matrix:")
+    print(f"             Predict cluster 1 Predict cluster 2")
+    print(f"Is cluster 1        {s1:<2}              {n-s1:<2}")
+    print(f"Is cluster 2        {n-s2:<2}              {s2:<2}")
+    print(f"\nSensitivity (Successfully predict cluster 1): {s1/n:.5f}")
+    print(f"Specificity (Successfully predict cluster 2): {s2/n:.5f}")
+
+
+def visualizeResult(X, w1, w2, n):
+    # draw Ground truth scatter plot
+    plt.subplot(131)
+    plt.title("Ground truth")
+    plt.scatter(X[0:n, 0], X[0:n, 1], c = 'r')
+    plt.scatter(X[n:2*n, 0], X[n:2*n, 1], c = 'b')
+
+    # Gradient descent
+    s1, s2, class1, class2 = classifyResult(X, w1)
+    print(f"Gradient descent:\n\nw:\n{float(w1[0]):15.10f}\n{float(w1[1]):15.10f}\n{float(w1[2]):15.10f}\n")
+    visualizeConfusionMatrix(s1, s2, n)
+
+    # draw Gradient descent
+    plt.subplot(132)
+    plt.title("Gradient descent")
+    plt.scatter(class1[:, 0], class1[:, 1], c = 'r')
+    plt.scatter(class2[:, 0], class2[:, 1], c = 'b')
+
+    print(f"\n----------------------------------------")
+    # Newton's method
+    s1, s2, class1, class2 = classifyResult(X, w2)
+    print(f"Newton's method:\n\nw:\n{float(w2[0]):15.10f}\n{float(w2[1]):15.10f}\n{float(w2[2]):15.10f}\n")
+    visualizeConfusionMatrix(s1, s2, n)
+
+    # draw Newton's method
+    plt.subplot(133)
+    plt.title("Newton's method")
+    plt.scatter(class1[:, 0], class1[:, 1], c = 'r')
+    plt.scatter(class2[:, 0], class2[:, 1], c = 'b')
+
+    plt.show()
+
 
 def Logistic_regression(n, mx1, vx1, my1, vy1, mx2, vx2, my2, vy2):
     # Generate D1 and D2
@@ -114,7 +214,12 @@ def Logistic_regression(n, mx1, vx1, my1, vy1, mx2, vx2, my2, vy2):
     X[n:2 * n, 0:2] = D2
     X[:, 2] = 1
     Y[n:2 * n, 0] = 1
-    w = Gradient_descent(X, Y)
-    print(w)
-    d = ml.generateUnivariateGaussianData(1,1);
-    pass
+    np.append([design_X, creatDesignMatrix(D1, 3)])
+    print(design_X.shape[0])
+    print(design_X)
+    np.concatenate([design_X, creatDesignMatrix(D2, 3)])
+    print(design_X.shape[0])
+    exit()
+    w1 = Gradient_descent(X, Y)
+    w2 = calculateNewtonRegression(X, Y, n)
+    visualizeResult(X, w1, w2, n)
